5df5c4f603a79c601603db3a6fa35b5b9335c617
Haroon Rasheed <kharoon274@gmail.com>
2026-07-18 23:28:57 +0300

    allowed host default removed

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -18,7 +18,7 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config(
     "ALLOWED_HOSTS",
-    default="127.0.0.1,localhost",
+    default="",
     cast=lambda v: [host.strip() for host in v.split(",")]
 )
 
